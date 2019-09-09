a1b5cfcabc0a21eaa44fe5fae72cc32689ef0687
Bruno Parga <brunoparga@gmail.com>
2019-09-09 15:19:24 -0300

    Initialize separate directory for assignments

---
+++ 3-navigation/app.js
@@ -0,0 +1,34 @@
+// IMPORTS SECTION
+// Node core module to deal with file paths
+const path = require('path')
+
+// NPM packages
+// The express.js framework
+const express = require('express')
+
+// App routes have been segregated into different files
+const routes = require('./routes/routes')
+
+// =================
+// =================
+
+// Main app object
+const app = express()
+
+// All middleware functions
+
+// Parse request body
+app.use(bodyParser.urlencoded({ extended: true }))
+// Serve static files, like CSS and browser JS
+app.use(express.static(path.join(__dirname, 'public')))
+// Prepend a path to these routes
+app.use('/admin', adminRoutes)
+// But not these
+app.use(shopRoutes)
+// Fall back to sending a 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+// App listens on port 3000
+app.listen(3000)
